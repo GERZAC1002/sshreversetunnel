01d59598b32cf30d61a01789081b54265cd8c49d
GERZAC1002 <gernot.zacharias@gmx.de>
2021-04-16 23:29:12 +0200

    minor change

--- sshreversetunnel.c
+++ sshreversetunnel.c
@@ -14,40 +14,42 @@ int main(){
 	char q = 10;
 	uint8_t *p;
 	p = (uint8_t*) &rip;
-	
+
 	printf("SSH REVERSE TUNNEL CREATOR\n\n");
-	
+
 	printf("Remote IP: ");
 	scanf("%" SCNu8 ".%" SCNu8 ".%" SCNu8 ".%" SCNu8,p,(p+1),(p+2),(p+3));
 	//SCNu8 -> for input of 8bit unsigned integers
 	//SCNd8 -> for input of 8bit signed integers
-	
+
 	printf("Remote Port: ");
 	scanf("%" SCNu16,&rprt);
 	//SCNu16 -> for input of 16bit unsigned integers
 	//SCNd16 -> for input of 16bit signed integers
-	
+
 	printf("Local Port: ");
 	scanf("%" SCNu16,&lprt);
-	
+
 	printf("Remote User: ");
 	scanf("%s",&user);
-	
+
 	printf("RIP: %d.%d.%d.%d RPRT: %d\n", *p,*(p+1),*(p+2),*(p+3),rprt);
 	printf("USER: %s LPRT: %d\n", user ,lprt);
-	
+
 	sprintf(command,"ssh -N -R %d.%d.%d.%d:%d:localhost:%d %s@%d.%d.%d.%d",
 			*p,*(p+1),*(p+2),*(p+3),rprt,lprt,user,*p,*(p+1),*(p+2),*(p+3));
 	printf("SSH Command: '%s'\n", command);
-	
+
 	printf("Execute command now? (y/n)\n");
 	while(q == 10){
 		q = getchar();
 	}
+
 	printf("======= SSH SESSION STARTED ======\n\n",q,q);
 	if(q == 'y'){
 		system(command);
+		printf("SSH Session terminated!\n");
 	}
-	
+
 	return 0;
 }
